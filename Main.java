c157417ed40c0ffe403c57ae9aadf7662bfd5083
JANGEUNWOO <42725476+JANGEUNWOO@users.noreply.github.com>
2018-09-15 10:13:22 +0900

    Login(90%완성)
    로그인 회원가입 DB연동 마무리했고 
    아이디 비밀번호 매칭시켜서 로그인 성공 메시지만 띄우면 됩니다.

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
-package unit1;
+package application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+	@Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(root);
+           // scene.getStylesheets().add(getClass().getResource("Login.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 	public static void main(String[] args) {
-		FrameSetting FS = new FrameSetting("그림판");
-
+		launch(args);
 	}
 
 }
