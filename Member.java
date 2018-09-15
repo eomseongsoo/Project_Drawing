15a3113951ee986e907fe48e7fb0888a0d06426a
JANGEUNWOO <42725476+JANGEUNWOO@users.noreply.github.com>
2018-09-15 10:05:39 +0900

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,64 @@
+package application;
+
+public class Member {
+	
+	private String id;
+	private String password;
+	private String name;
+	private String addr;
+	private String phone;
+
+	public Member() {
+
+	}
+
+	public Member(String id, String password, String name, String addr, String phone) {
+
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.addr = addr;
+		this.phone = phone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
