00a44868714f8e64ded540d93fe04465ea8f7658
JANGEUNWOO <42725476+JANGEUNWOO@users.noreply.github.com>
2018-09-17 11:33:36 +0900

    로그인 기능
    완성..

--- JoinController.java
+++ JoinController.java
@@ -1,11 +1,14 @@
 package application;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -32,17 +35,23 @@ public class JoinController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
 		btnOk.setOnAction(event -> handleFormOK(event));
+		btnCancel.setOnAction(e -> handleFormCancel(e));
 	}
 
 	public void handleFormOK(ActionEvent event) {
 
-
 		MemberDAO mmd = new MemberDAO();
-		mmd.insertMember(new Member(id_tf.getText(), password_tf.getText(), 
-				name_tf.getText(), address_tf.getText(), phone_tf.getText()));
-
+		mmd.insertMember(new Member(id_tf.getText(), password_tf.getText(), name_tf.getText(), address_tf.getText(),
+				phone_tf.getText()));
+		((Node) (event.getSource())).getScene().getWindow().hide();
 
 	}
-	
+
+	public void handleFormCancel(ActionEvent e) {
+
+		
+		((Node) (e.getSource())).getScene().getWindow().hide();
+		
+	}
 
 }
