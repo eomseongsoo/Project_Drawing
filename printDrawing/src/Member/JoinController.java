9c10e328a8e1b4f26a5721ad7e6ac5c487f5b26b
xce223451 <42724561+xce223451@users.noreply.github.com>
2018-09-18 02:36:02 +0000

    Revert "Revert "Jangeunwoo login""

---
+++ printDrawing/src/Member/JoinController.java
@@ -0,0 +1,48 @@
+package Member;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class JoinController implements Initializable {
+
+	@FXML
+	private TextField id_tf;
+	@FXML
+	private PasswordField password_tf;
+	@FXML
+	private TextField name_tf;
+	@FXML
+	private TextField address_tf;
+	@FXML
+	private TextField phone_tf;
+
+	@FXML
+	private Button btnOk;
+	@FXML
+	private Button btnCancel;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		btnOk.setOnAction(event -> handleFormOK(event));
+	}
+
+	public void handleFormOK(ActionEvent event) {
+
+
+		MemberDAO mmd = new MemberDAO();
+		mmd.insertMember(new Member(id_tf.getText(), password_tf.getText(), 
+				name_tf.getText(), address_tf.getText(), phone_tf.getText()));
+
+
+	}
+	
+
+}
